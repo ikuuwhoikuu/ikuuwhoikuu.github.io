002c2d2ce741df9046eb75c7861b64c9e18f0abb
micuat <micuat@gmail.com>
2018-03-17 21:07:57 -0400

    add front animation; refactoring libs

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var s = 64;
+var ck = 255;
+var cn = 0;
+var start = false;
+
+var names = ["Practicing Caring",
+  "Chaos",
+  "Buffett",
+  "History of the Building",
+  "Tobacco Plant",
+  "Landscape and Environment",
+  "General Introduction",
+  "Butterflies",
+  "Microbiome",
+  "History of the Building",
+  "Tea Plant",
+  "Coffee Plant",
+  "Thermal Sounds",
+  "QR Code",
+  "Open Weather Map",
+  "Open Sound Control",
+  "Michael Scerbo",
+  "Marcel Smit",
+  "Jur de Vries",
+  "Naoto Hieda"
+];
+
+function setup() {
+  let canvas = createCanvas(600, 800);
+  canvas.parent("container");
+}
+
+function draw() {
+  background(255);
+
+  // translate(width/2, height/2);
+  textAlign(CENTER);
+
+  if (start) {
+    if (s < 500) {
+      s += 20;
+    }
+    if (ck > 0) {
+      ck -= 20;
+    } else if (cn < 255) {
+      cn += 2;
+    }
+  }
+  fill(0, ck);
+  textSize(s);
+  text('KUU', width / 2, height / 2 + s / 2);
+
+  fill(0, cn);
+  textSize(32);
+  let i = 0;
+  for (let name of names) {
+    text(name, width / 2, (i + 1) * 32);
+    i++;
+  }
+}
+
+function mousePressed() {
+  start = true;
+}
