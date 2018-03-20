d99aba003eebfdbbacdda6f179f78dc21e3833df
micuat <micuat@gmail.com>
2018-03-20 16:07:36 -0400

    buffet copy from food

--- buffett-food-for-guts/sketch.js
+++ buffett-food-for-guts/sketch.js
@@ -1,57 +1,84 @@
 // Daniel Shiffman
 // http://codingtra.in
-// Butterfly Wings
-// Video: [coming soon]
+// http://patreon.com/codingtrain
+// Code for this video: https://youtu.be/Cl_Gjj80gPE
 
 // instance mode by Naoto Hieda
 
-var yoff = 0;
-var sc = 64;
-var ck = 0;
-var cn = 0;
-var loading = false;
-var start = false;
+var yoff = 0.0;
+var points = [];
 
 var s = function (sketch) {
   sketch.setup = function () {
     sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-    sketch.background(0);
+    sketch.noiseDetail(1,0.5);
+    let xoff = 0;
+    for (let a = 0; a < 1; a += 0.02) {
+      points.push({ x: 0, y: 0, r: 0, next: null });
+    }
+    for (let i = 0; i < points.length; i++) {
+      points[i].next = points[i + 1];
+    }
+    points[points.length - 1].next = points[0];
 
     setTimeout(function() {
       $("#page").animate({opacity: '0.95'}, 2000);
       $("canvas").animate({opacity: '0.05'}, 2000);
     }, 3000);
-    loading = true;
   }
 
   sketch.draw = function () {
-    sketch.background(255);
+    sketch.background(0);
 
-    // translate(width/2, height/2);
-    sketch.textAlign(sketch.CENTER);
-  
-    if (loading) {
-      if(ck < 255) {
-        ck += 4;
+    sketch.translate(sketch.width / 2, sketch.height / 2);
+
+    let radius = 300;
+
+    let xoff = 0;
+    // let opoints = points.slice();
+    let l = points.length;
+    for (let i = 0; i < points.length; i++) {
+      let ppn = sketch.map(sketch.noise(xoff, yoff), 0, 1, -25, 25) * 2;
+      if (i == l - 1) {
+        ppn = ppn * 0.5 + 0.5 * sketch.map(sketch.noise(0, yoff), 0, 1, -25, 25);
       }
-      else {
-        loading = false;
-        start = true;
+      // let pn = ppn * 0.5 + opoints[(i-1+l)%l] * 0.25 + opoints[(i+1)%l] * 0.25;
+      // let po = opoints[i] * 0.5 + opoints[(i-1+l)%l] * 0.25 + opoints[(i+1)%l] * 0.25;
+      // points[i] = sketch.lerp(po, pn, 0.5);
+      // let offset = points[i];
+      let offset = ppn;
+      let r = radius + offset * 1;
+      let x = r * sketch.cos(i / points.length * sketch.TWO_PI);
+      let y = r * sketch.sin(i / points.length * sketch.TWO_PI);
+      points[i].x = x;
+      points[i].y = y;
+      points[i].r = offset;
+      xoff += 0.1;
+    }
+    // sketch.noFill();
+    // sketch.stroke(255);
+    sketch.noStroke();
+    sketch.colorMode(sketch.HSB);
+    for (let i = 0; i < points.length; i++) {
+      for (let j = 0; j < 8; j++) {
+        let p = sketch.map(j, 0, 8, 1, 0.8);
+        let s = 255 - j * 36;// * (2+sketch.cos(0.4 + i / points.length * sketch.TWO_PI));
+        sketch.fill(points[i].r*2 + 100, s, j * 50, 255);
+        sketch.beginShape();
+        sketch.vertex(0, 0);
+        sketch.vertex(points[i].x * p, points[i].y * p);
+        sketch.vertex(points[i].next.x * p, points[i].next.y * p);
+        sketch.vertex(0, 0);
+        sketch.endShape();
       }
     }
-    if (start) {
-      if (sc < 500) {
-        sc += 10;
-      }
-      if (ck > 0) {
-        ck -= 20;
-      } else if (cn < 255) {
-        cn += 2;
-      }
+    if(sketch.millis() * 0.001 % 4 < 0.75) {
+      yoff += 0.2;
     }
-    sketch.fill(0, ck);
-    sketch.textSize(sc);
-    sketch.text('BUFFET', sketch.width / 2, sketch.height / 2 + sc / 2);  
+    else {
+      yoff += 0.05;
+    }
+    
   }
 
 };
